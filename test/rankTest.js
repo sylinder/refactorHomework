10044ccb5b850475e60b702ababe084e96035116
caps <yzsylinder@163.com>
2020-08-28 21:10:59 +0800

    [caps] test: add voyageProfitFactor test case

--- test/rankTest.js
+++ test/rankTest.js
@@ -70,3 +70,184 @@ rankTest('voyageRisk case 6. should return 9 when voyageRisk given zone is china
   t.is(result, 9);
 })
 
+rankTest('voyageProfitFactor case 7. should return 2 when voyageProfitFactor given zone is west-indies and history length is 2 and voyage length is 10.', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },
+  ]
+
+  const result = voyageProfitFactor(voyage, history)
+
+  t.is(result, 2);
+})
+
+rankTest('voyageProfitFactor case 8. should return 1 when voyageProfitFactor given zone is west-indies and history length is 2 and voyage length is 15.', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 15,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },{
+      zone: 'west-indies',
+      profit: 15,
+    },
+  ]
+
+  const result = voyageProfitFactor(voyage, history)
+
+  t.is(result, 1);
+})
+
+rankTest('voyageProfitFactor case 9. should return 2 given zone is west-indies and history length is 9 and voyage length is 15.', t => {
+  const voyage = {
+    zone: 'west-indies',
+    length: 15,
+  };
+  const history = [
+    {
+      zone: 'east-indies1',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies2',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies3',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies4',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies5',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies6',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies7',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies8',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies9',
+      profit: 5,
+    },
+  ]
+
+  const result = voyageProfitFactor(voyage, history)
+
+  t.is(result, 2);
+})
+
+rankTest('voyageProfitFactor case 10. should return 3 given zone is china and history no hasChina and history length is 1 and voyage length is 10.', t => {
+  const voyage = {
+    zone: 'china',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies1',
+      profit: 5,
+    },
+  ]
+
+  const result = voyageProfitFactor(voyage, history)
+
+  t.is(result, 3);
+})
+
+rankTest('voyageProfitFactor case 11. should return 7 given zone is china and history hasChina and history length is 11 and voyage length is 19.', t => {
+  const voyage = {
+    zone: 'china',
+    length: 19,
+  };
+  const history = [
+    {
+      zone: 'east-indies1',
+      profit: 5,
+    },
+    {
+      zone: 'china',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies2',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies3',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies4',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies5',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies6',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies7',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies8',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies9',
+      profit: 5,
+    },
+    {
+      zone: 'east-indies10',
+      profit: 5,
+    },
+
+
+  ]
+
+  const result = voyageProfitFactor(voyage, history)
+
+  t.is(result, 7);
+})
+
+rankTest('voyageProfitFactor case 12. should return 4 given zone is east-indies and history no hasChina and history length is 1 and voyage length is 10.', t => {
+  const voyage = {
+    zone: 'east-indies',
+    length: 10,
+  };
+  const history = [
+    {
+      zone: 'east-indies',
+      profit: 5,
+    },
+  ]
+
+  const result = voyageProfitFactor(voyage, history)
+
+  t.is(result, 3);
+})
